8c5c566816cbe6f83e21724ea3bdbc54e8ba0b7e
Tyag0 <109483470+Tyag0@users.noreply.github.com>
2022-10-09 14:21:08 -0500

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -1,40 +1,50 @@
 import time
 from random import randint
 
+#Se define la forma estándar del tablero
+table = [["|","1","2","3","4","5","6","7","|"],
+        ["+","-","-","-","-","-","-","-","+"],
+        ["|",".",".",".",".",".",".",".","|"],
+        ["|",".",".",".",".",".",".",".","|"],
+        ["|",".",".",".",".",".",".",".","|"],
+        ["|",".",".",".",".",".",".",".","|"],
+        ["|",".",".",".",".",".",".",".","|"],
+        ["|",".",".",".",".",".",".",".","|"],
+        ["+","-","-","-","-","-","-","-","+"]]
 
+#Se define las características de los jugadores
 class jugador():
     def __init__(self, nombre, ficha):
         self.nombre = nombre
         self.ficha = ficha
 
-def tablero():
-  print("")
-  print("+","+","+","+","+","+","+","+",sep="-")
-  print("|1|2|3|4|5|6|7|")
-  print("+","+","+","+","+","+","+","+",sep="-")
-  print("|","|", "|", "|", "|","|","|","|")
-  print("+","+","+","+","+","+","+","+",sep="-")
-  print("|","|", "|", "|", "|","|","|","|")
-  print("+","+","+","+","+","+","+","+",sep="-")
-  print("|","|", "|", "|", "|","|","|","|")
-  print("+","+","+","+","+","+","+","+",sep="-")
-  print("|","|", "|", "|", "|","|","|","|")
-  print("+","+","+","+","+","+","+","+",sep="-")
-  print("|","|", "|", "|", "|","|","|","|")
-  print("+","+","+","+","+","+","+","+",sep="-")
-  print("|","|", "|", "|", "|","|","|","|")
-  print("+","+","+","+","+","+","+","+",sep="-")
-  print("")
-    #En proceso...
+    def turno(self):
+        global table
 
-def turno(jugador):
-    row = input(f"{jugador.nombre}, indica un número de columna o pulsa [S] para tentar a la suerte: ")
-    if row=="S" or row=="s":
-        row = randint(1,7)
-        print(f"mmm...suerte con eso, se eligió aleatoriamente la columna {row} para tu ficha")
-    else:
-        row = int(row)
-    #En proceso...
+        row = input(f"{self.nombre}, indica un número de columna o pulsa [S] para tentar a la suerte: ")
+        if row=="S":
+            row = randint(1,7)
+            print(f"mmm...suerte con eso, se eligió aleatoriamente la columna {row} para tu ficha")
+        else:
+            row = int(row)
+        
+        for i in range(7):
+            index = 7-i
+            if table[index][row] == ".":
+                table[index][row] = self.ficha
+                break
+        
+        return tablero()
+
+#Función que dibuja el tablero
+def tablero():
+    for i in range(9):
+        for j in range(9):
+            if j==8:
+                print(table[i][j])
+            else:
+                print(table[i][j], end="")
+
 
 play = True
 try:
@@ -42,7 +52,7 @@ try:
 
         #INICIO DE PARTIDA#
         print(4*"*","CUATRO SEGUIDAS",4*"*")
-        for i in range(1,3):
+        for i in range(1,3,1):
             nombre = input(f"Por favor indique nombre de participante #{i}: ")
             if i==1:
                 ficha = input(f"{nombre}, por favor indica con qué ficha deseas jugar [X] o [O]: ")
@@ -54,14 +64,14 @@ try:
                     ficha = "X"
                 print(f"{nombre}, te toca jugar con la siguiente ficha: {ficha}")
                 jugador2 = jugador(nombre, ficha)
-        jugadores = [jugador1, jugador2] #En proceso
+        jugadores = [jugador1, jugador2]
         
         #SELECCION DE QUIEN EMPIEZA
         print("")
         inicia = randint(0,1)
-        time.sleep(3)
+        time.sleep(1)
         print("Lanzando una moneda al aire para determinar quién inicia la partida...")
-        time.sleep(4)
+        time.sleep(3)
         if inicia == 0:
             print(f"La partida la inicia {jugador1.nombre}")
         else:
@@ -71,18 +81,23 @@ try:
         Win = False
         time.sleep(2)
         tablero()
-        while not Win:
-            if inicia==0:
-                turno(jugador1)
-            elif inicia==1:
-                turno(jugador2)
-            for i in jugadores:
-                turno(i) #En proceso...
-            Win = True #En proceso...
+        #Primera ronda de juego
+        if inicia==0:
+            jugador1.turno()
+            jugador2.turno()
+        elif inicia==1:
+            jugador2.turno()
 
+        #Ciclo para los turnos
+        while not Win:
+            for i in jugadores:
+                i.turno()
+            Win = True
+
+        #Pregunta para volver a jugar
         continua = input("¿Desean volver a jugar? [Si] / [No]:")
         if continua == "No" or continua == "no":
             print("¡Gracias por jugar!")
             play = False
 except:
-    pass #En proceso...
+    pass
